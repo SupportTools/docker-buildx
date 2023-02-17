b6eb110dd521f0cce2eafb49fdaa92caceb7a7aa
Matthew Mattox <mmattox@support.tools>
2023-02-16 23:49:32 -0600

    Removing flag

--- docker-build.sh
+++ docker-build.sh
@@ -9,7 +9,7 @@ GIT_COMMIT=$(git rev-list -1 HEAD)
 echo "Setting up buildx..."
 docker buildx version
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --use
+docker buildx create
 
 echo "Building..."
 docker buildx build \
